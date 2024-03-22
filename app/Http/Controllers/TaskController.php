2c28a1ac820f1c1050b81d81e67d7b0c0e4c7492
Elton Sidney Pedro de Sousa <sidneymelodia@Eltons-MBP.home>
2024-03-22 13:02:07 +0200

    initial commit with task items

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $tasks = $request->user()->tasks;
+
+        return view('task.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+    
+        $request->user()->tasks()->create([ 'name' => $request->name ]);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        
+        $task->delete();
+        return redirect()->back();
+    }
+}
---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container">
+
+        <div class="row">
+            <div class="card mb-3">
+                <div class="card-header">New task</div>
+
+                <div class="card-body">
+                    <form method="post" action="{{ route('task.store') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Task</label>
+                            <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="nameHelp">
+                            @error('name')
+                                <div class="invalid-feedback">
+                                    Name is a required field
+                                </div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="card">
+                <div class="card-body"></div>
+                    @if($tasks)
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Created</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($tasks as $task)
+                                    <tr>
+                                        <th scope="row">{{ $task->id }}</th>
+                                        <td>{{ $task->name }}</td>
+                                        <td>{{ $task->created_at->diffForHumans() }}</td>
+                                        <td>
+                                            <form method="post" action="{{ route('task.destroy', ['task' => $task->id]) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-danger" type="submit">Delete</button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('task.show', $task->id) }}" class="btn-btn-warning">Edit</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+        
+                            </tbody>
+                        </table>
+                    @else
+                        <h1 class="text-center">No tasks added</h1>
+                    @endif
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+@endsection
